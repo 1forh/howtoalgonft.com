9eb2ea0fd7aabf30a6293b57ea4d2fbdd020b77c
1forh <1forhlol@gmail.com>
2021-12-30 21:18:28 -0500

    ✏️ fixes typo

--- components/Helmet.js
+++ components/Helmet.js
@@ -16,7 +16,7 @@ function Helmet({ title, description }) {
       <meta name='twitter:description' content={description} />
       <meta
         name='twitter:image'
-        content={'https://www.howtoalgonfts.com/twitter-share-image.png'}
+        content={'https://www.howtoalgonft.com/twitter-share-image.png'}
       />
       <meta name='twitter:card' content='summary_large_image' />
 
