fda21a12c966b7f5810f6e05b1b3863046cff0ad
1forh <1forhlol@gmail.com>
2021-12-31 19:17:55 -0500

    ✨ adds donation link

--- components/Footer.js
+++ components/Footer.js
@@ -1,35 +1,16 @@
+import Link from 'next/link';
 import React from 'react';
-import Modal from './Modal';
+import DonationLink from './DonationLink';
 import TwitterLink from './TwitterLink';
 
 function Footer() {
-  const [modalOpen, setModalOpen] = React.useState(false);
-
   return (
-    <div className='pt-12 pb-12 space-y-8 text-center lg:pt-48'>
+    <div className='pt-12 pb-12 space-y-8 text-center lg:pt-32'>
       <div className='flex flex-col items-center justify-center space-y-10'>
         <p className='flex justify-center'>
           <TwitterLink />
         </p>
-        <button
-          className='flex items-center justify-center px-5 py-2 space-x-3 text-sm font-medium transition-colors duration-150 bg-green-900 bg-opacity-50 border border-green-800 rounded-md text-green-50 hover:bg-opacity-75'
-          type='button'
-          onClick={() => setModalOpen(true)}
-        >
-          <span className='block w-3 h-3'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 113 113.4'
-              className='w-full h-full'
-            >
-              <polygon
-                fill='currentColor'
-                points='19.6 113.4 36 85 52.4 56.7 68.7 28.3 71.4 23.8 72.6 28.3 77.6 47 72 56.7 55.6 85 39.3 113.4 58.9 113.4 75.3 85 83.8 70.3 87.8 85 95.4 113.4 113 113.4 105.4 85 97.8 56.7 95.8 49.4 108 28.3 90.2 28.3 89.6 26.2 83.4 3 82.6 0 65.5 0 65.1 0.6 49.1 28.3 32.7 56.7 16.4 85 0 113.4 19.6 113.4'
-              ></polygon>
-            </svg>
-          </span>
-          <span>Donate</span>
-        </button>
+        <DonationLink />
       </div>
       <p>
         Created by{' '}
@@ -40,10 +21,6 @@ function Footer() {
           Minner.Algo
         </a>
       </p>
-
-      <Modal open={modalOpen} setOpen={setModalOpen}>
-        testing
-      </Modal>
     </div>
   );
 }
