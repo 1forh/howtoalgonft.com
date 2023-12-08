978477a90471dcb15af91550cf88ae4ec87638e9
1forh <1forhlol@gmail.com>
2023-12-08 01:52:08 -0500

    Add generateMetadata function to page.tsx

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -6,8 +6,42 @@ import Cta from '../_components/Cta';
 import Gradients from '../_components/Gradients';
 import LogoCloud from '../_components/LogoCloud';
 import Features from '../_components/sections/Features';
+import { Metadata, ResolvingMetadata } from 'next';
+import { SITE_NAME } from '@/constants';
 
 export const revalidate = 300;
+
+export async function generateMetadata(
+  { }, // params
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const payload = await getPayloadClient();
+
+  // get home page
+  const homePageResp = await payload.find({
+    collection: 'pages',
+    where: {
+      slug: {
+        equals: 'home',
+      }
+    }
+  });
+
+  const homePage = homePageResp.docs[0];
+  const meta = homePage.meta;
+  if (!meta) return {
+    title: SITE_NAME,
+  }
+
+  return {
+    title: meta.title,
+    description: meta.description,
+    openGraph: {
+      // @ts-ignore
+      images: meta.image?.url
+    },
+  }
+}
 
 export default async function Home() {
   const payload = await getPayloadClient();
