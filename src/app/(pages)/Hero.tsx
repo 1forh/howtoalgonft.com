eab346abd2f3e92dbf7ba2b6d1b1f470dc458a76
1forh <1forhlol@gmail.com>
2023-12-08 00:50:46 -0500

    Update Hero component styles

--- src/app/(pages)/Hero.tsx
+++ src/app/(pages)/Hero.tsx
@@ -5,9 +5,9 @@ type Props = {};
 const Hero = (props: Props) => {
   return (
     <PageContainer>
-      <div className='max-w-2xl mb-16 md:mb-32'>
+      <div className='max-w-xl mb-16 md:mb-32'>
         <h1 className='text-4xl font-bold tracking-tight text-gray-800 dark:text-gray-100 sm:text-5xl'>Dive into the world of Algorand NFTs</h1>
-        <p className='mt-6 text-base text-gray-600 dark:text-gray-400'>
+        <p className='mt-6 text-gray-600 dark:text-gray-400'>
           Explore our resources, tutorials, and community insights to become an Algorand NFT expert. Whether you're a beginner curious about the world
           of NFTs or an experienced creator looking to expand your knowledge, we've got you covered.
         </p>
