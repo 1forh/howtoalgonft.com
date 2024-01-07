3e3f67675f7ae0d5c825fbb176dc4aa64e1c326c
1forh <1forhlol@gmail.com>
2024-01-07 17:19:33 -0500

    Add MediaCollection import to payload.config.ts

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -4,12 +4,12 @@ import { slateEditor } from '@payloadcms/richtext-slate';
 import path from 'path';
 import { buildConfig } from 'payload/config';
 import CategoriesCollection from '@/payload/collections/categories';
-import MediaCollection from '@/payload/collections/media';
 import PagesCollection from '@/payload/collections/pages';
 import PostsCollection from '@/payload/collections/posts';
 import UsersCollection from '@/payload/collections/users';
 import SeoPlugin from '@/payload/plugins/seo';
 import CloudStoragePlugin from '@/payload/plugins/storage';
+import MediaCollection from '@/payload/collections/media';
 
 export default buildConfig({
   plugins: [CloudStoragePlugin, SeoPlugin],
